af42086ddcd76c2ad0aee8c1bae16bb64712eb9d
Rishit Umesh Padagatti <150834740+RishitUmeshPadagatti@users.noreply.github.com>
2024-05-13 23:42:05 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
-const JWT_PASSWORD = "uhmm this is my JWT password"
-const DB_CONNECTION_STRING = "mongodb+srv://admin:OI3dW2ikR12Gi4wx@cluster0.afupkaz.mongodb.net/todo_with_users_project"
+const JWT_PASSWORD = 
+const DB_CONNECTION_STRING = 
 
 module.exports = {
     JWT_PASSWORD,
